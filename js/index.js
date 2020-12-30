6f41763ca4149410c0e082dea985b50a801ba8b3
hyenjung <beibby@naver.com>
2020-12-30 12:22:43 +0900

    c

--- js/index.js
+++ js/index.js
@@ -7,3 +7,17 @@ var swiper = new Swiper('.slide.swiper-container' ,{
 })
 }
 Slide();
+
+function navi() {
+	$("header nav ul.gnb >li:last-child").mouseenter(function(){
+		$("header nav ul.gnb >li:last-child").find("li").show();
+	});
+	$("header nav ul.gnb >li:last-child").mouseleave(function(){
+		$("header nav ul.gnb >li:last-child").find("li").hide();
+	});
+  $(".search > i.last").click(function(){
+	$("header nav ul.gnb").find("li:not(:nth-child(1))").toggle();
+	$("header nav ul.gnb").find("li").eq(5).stop().hide();
+});
+}
+navi();
