0fcb1a5d775bfae14ca8e0f35e77adb0ca53e80c
hyenjung <beibby@naver.com>
2020-12-31 17:12:29 +0900

    fds

--- public/js/index.js
+++ public/js/index.js
@@ -8,16 +8,40 @@ var swiper = new Swiper('.slide.swiper-container' ,{
 }
 Slide();
 
-function navi() {
-	$("header nav ul.gnb >li:last-child").mouseenter(function(){
-		$("header nav ul.gnb >li:last-child").find("li").show();
-	});
-	$("header nav ul.gnb >li:last-child").mouseleave(function(){
-		$("header nav ul.gnb >li:last-child").find("li").hide();
-	});
-  $(".search > i.last").click(function(){
-	$("header nav ul.gnb").find("li:not(:nth-child(1))").toggle();
-	$("header nav ul.gnb").find("li").eq(5).stop().hide();
-});
+/********* 이벤트선언 **********/
+$(".header-wrapper .bar-wrap").click(onBarWrap);
+
+
+/********* 이벤트콜백 **********/
+function onBarWrap() {
+	$(".header-wrapper .mo-navi-wrapper").toggleClass('active');
 }
-navi();
+
+/********* 카카오지도 **********/
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	mapOption = { 
+			center: new kakao.maps.LatLng(37.498762, 127.026782), // 지도의 중심좌표
+			level: 3 // 지도의 확대 레벨
+	};
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(37.498762, 127.026782); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+		position: markerPosition
+});
+
+function setCenter() {            
+	// 이동할 위도 경도 위치를 생성합니다 
+	var moveLatLon = new kakao.maps.LatLng(37.498762, 127.026782);
+	
+	// 지도 중심을 이동 시킵니다
+	map.setCenter(moveLatLon);
+}
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+map.setZoomable(false); 
